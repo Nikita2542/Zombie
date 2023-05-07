610c98c3c3fddadf690e81805d963b0196d3c3fe
Nikita2542 <nikitagameplay25@gmail.com>
2023-05-07 21:58:39 +0300

    main

--- ZHH/Assets/Scripts/Gun_Script/GunOptionsMain.cs
+++ ZHH/Assets/Scripts/Gun_Script/GunOptionsMain.cs
@@ -13,14 +13,18 @@ public class GunOptionsMain : MonoBehaviour
     public Gun_Zombie avtomatZombie;
     [Header("Скрипт для пулемета")]
     public Gun_Zombie pulemetZombie ;
+    [Header("Скрипт для снайперки")]
+    public Gun_Zombie sniperZombie;
 
     [HideInInspector] public bool gunAvtomat;
     [HideInInspector] public bool gunSlizator;
     [HideInInspector] public bool gunPulemet;
+    [HideInInspector] public bool gunSniper;
 
     [HideInInspector] public bool gunActiv;
     [HideInInspector] public bool slizatorActiv;
     [HideInInspector] public bool PulemetActiv;
+    [HideInInspector] public bool sniperActiv;
 
 
     // Start is called before the first frame update
@@ -29,12 +33,15 @@ public class GunOptionsMain : MonoBehaviour
         textGun[0].SetActive(true);
         textGun[1].SetActive(false);
         textGun[2].SetActive(false);
+        textGun[3].SetActive(false);
         gunAll[0].gameObject.SetActive(true);
         gunAll[1].gameObject.SetActive(false);
         gunAll[2].gameObject.SetActive(false);
+        gunAll[3].gameObject.SetActive(false);
         gunAvtomat = false;
         gunSlizator = false;
         gunPulemet = false;
+        gunSniper = false;
 
 
 
@@ -50,6 +57,7 @@ public class GunOptionsMain : MonoBehaviour
         {
             avtomatZombie.enabled = true;
             pulemetZombie.enabled = false;
+            sniperZombie.enabled = false;
             gunAll[0].gameObject.SetActive(true);
             textGun[0].gameObject.SetActive(true);
 
@@ -57,10 +65,14 @@ public class GunOptionsMain : MonoBehaviour
             textGun[1].gameObject.SetActive(false);
             gunAll[2].gameObject.SetActive(false);
             textGun[2].gameObject.SetActive(false);
-            
+
+            gunAll[3].gameObject.SetActive(false);
+            textGun[3].gameObject.SetActive(false);
+
             gunAvtomat = true;
             gunSlizator = false;
             gunPulemet = false;
+            gunSniper = false;
         }
        
         if (slizatorActiv == true)
@@ -74,25 +86,65 @@ public class GunOptionsMain : MonoBehaviour
 
             gunAll[2].gameObject.SetActive(false);
             textGun[2].gameObject.SetActive(false);
-            
+
+            gunAll[3].gameObject.SetActive(false);
+            textGun[3].gameObject.SetActive(false);
+
             gunSlizator = true;
             gunAvtomat = false;
             gunPulemet = false;
+            gunSniper = false;
 
         }
        if(PulemetActiv == true)
         {
             avtomatZombie.enabled = false;
             pulemetZombie.enabled = true;
+            sniperZombie.enabled = false;
             gunAll[2].gameObject.SetActive(true);
             textGun[2].gameObject.SetActive(true);
 
             gunAll[0].gameObject.SetActive(false);
             textGun[0].gameObject.SetActive(false);
+
             gunAll[1].gameObject.SetActive(false);
             textGun[1].gameObject.SetActive(false);
 
+            gunAll[3].gameObject.SetActive(false);
+            textGun[3].gameObject.SetActive(false);
+
             gunPulemet = true;
+            gunAvtomat = false;
+            gunSlizator = false;
+            gunSniper = false;
+        }
+        if (sniperActiv == true)
+        {
+            //- Скрипт - TRUE ---------------------------
+            sniperZombie.enabled = true;
+
+            //- Скрипт - False ---------------------------
+            avtomatZombie.enabled = false;
+            pulemetZombie.enabled = false;
+            
+            //- Объект - TRUE ---------------------------
+            gunAll[3].gameObject.SetActive(true);      
+
+            //- Объекты - FALSE ---------------------------
+            gunAll[0].gameObject.SetActive(false);
+            gunAll[1].gameObject.SetActive(false);
+            gunAll[2].gameObject.SetActive(false);
+
+            //- Текст - TRUE ---------------------------
+            textGun[3].gameObject.SetActive(true);
+
+            //- Текст - FALSE ---------------------------
+            textGun[0].gameObject.SetActive(false);           
+            textGun[1].gameObject.SetActive(false);
+            textGun[2].gameObject.SetActive(false);
+
+            gunSniper = true;
+            gunPulemet = false;
             gunAvtomat = false;
             gunSlizator = false;
         }
--- ZHH/Assets/WeaponWheelButtonController.cs
+++ ZHH/Assets/WeaponWheelButtonController.cs
@@ -76,6 +76,12 @@ public class WeaponWheelButtonController : MonoBehaviour
                 itemText.gameObject.transform.position = new Vector3(gameObject.transform.position.x - distantText, gameObject.transform.position.y, gameObject.transform.position.z);
                 ActivatePulemet();
             }
+            if (ID == 5)
+            {
+                ImageBack.gameObject.transform.position = new Vector3(gameObject.transform.position.x - distantImage, gameObject.transform.position.y, gameObject.transform.position.z);
+                itemText.gameObject.transform.position = new Vector3(gameObject.transform.position.x - distantText, gameObject.transform.position.y, gameObject.transform.position.z);
+                ActivateSniper();
+            }
         }
         
     }
@@ -110,29 +116,54 @@ public class WeaponWheelButtonController : MonoBehaviour
     public void ActivateGun()
     {
         gunMain.gunActiv = true;
+
+        // - False -
         gunMain.slizatorActiv = false;
         ammo.craftActive = false;
         gunMain.PulemetActiv = false;
+        gunMain.sniperActiv = false;
     }
     public void ActivateSlizator()
     {
         gunMain.slizatorActiv = true;
+
+        // - False -
         gunMain.gunActiv = false;
         ammo.craftActive = false;
         gunMain.PulemetActiv = false;
+        gunMain.sniperActiv = false;
     }
     public void ActivateCraft()
     {
         ammo.craftActive = true;
-        gunMain.slizatorActiv = false;
         gunMain.gunActiv = true;
+
+        // - False -
+        gunMain.slizatorActiv = false; 
         gunMain.PulemetActiv = false;
+        gunMain.sniperActiv = false;
     }
     public void ActivatePulemet()
     {
         
         gunMain.PulemetActiv = true;
+
+        // - False -
         ammo.craftActive = false;
+        gunMain.slizatorActiv = false;
+        gunMain.gunActiv = false;
+        gunMain.sniperActiv = false;
+
+    }
+    public void ActivateSniper()
+    {
+
+        gunMain.sniperActiv = true;
+
+        // - False -
+        ammo.craftActive = false;
+
+        gunMain.PulemetActiv = false;    
         gunMain.slizatorActiv = false;
         gunMain.gunActiv = false;
 
